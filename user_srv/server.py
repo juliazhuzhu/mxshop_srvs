3e0100f8110bb361dd0e43fc90eedf668f20478f
xiaoye <zhuyy@hexmeet.com>
2022-09-07 14:49:27 +0800

    to register in consul

--- user_srv/server.py
+++ user_srv/server.py
@@ -2,9 +2,12 @@ import os.path
 import signal
 import sys
 import os
+import uuid
+
 import grpc
 from concurrent import futures
 import argparse
+import socket
 
 BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 print(BASE_DIR)
@@ -15,11 +18,24 @@ from user_srv.handler.user import UserServicer
 from common.grpc_health.v1 import health_pb2, health_pb2_grpc
 from common.grpc_health.v1 import health
 from loguru import logger
+from common.register import consul
+from user_srv.settings import settings
+from functools import partial
 
-
-def on_exit(sigo, frame):
-    logger.info("进程中断")
+def on_exit(sigo, frame, service_id):
+    logger.info(f"注销{service_id}服务")
+    register = consul.ConsulRegister(settings.CONSUL_HOST, settings.CONSUL_PORT)
+    register.deregister(service_id)
+    logger.info("注销成功")
     sys.exit(0)
+
+
+def get_free_tcp_port():
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind(("",0))
+    addr, port = tcp.getsockname()
+    tcp.close()
+    return port
 
 
 def serve():
@@ -32,11 +48,14 @@ def serve():
     parser.add_argument('--port',
                         nargs="?",
                         type=int,
-                        default=50051,
+                        default=0,
                         help="the listening port")
 
     args = parser.parse_args()
     print(args)
+
+    if args.port == 0:
+        args.port = get_free_tcp_port()
 
     logger.add("logs/user_srv_{time:YYYY-MM-DD_HH:mm:ss}.log", rotation="10 MB")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -48,13 +67,24 @@ def serve():
 
     server.add_insecure_port(f'{args.ip}:{args.port}')
     logger.info(f"启动服务 {args.ip}:{args.port}")
+    service_id = str(uuid.uuid1())
     '''
         SIGINT ctrl+c
         SIGTERM kill 发出的软件终止命令
     '''
-    signal.signal(signal.SIGINT, on_exit)
-    signal.signal(signal.SIGTERM, on_exit)
+    signal.signal(signal.SIGINT, partial(on_exit,service_id=service_id))
+    signal.signal(signal.SIGTERM, partial(on_exit,service_id=service_id))
     server.start()
+
+    logger.info(f"服务注册开始")
+
+    register = consul.ConsulRegister(settings.CONSUL_HOST, settings.CONSUL_PORT)
+
+    if not register.register(name=settings.SERVICE_NAME, id=service_id, address=args.ip, port=args.port,
+                             tags=settings.SERVICE_TAGS, check=None):
+        logger.warning(f"服务注册失败")
+        sys.exit(0)
+    logger.info(f"服务注册成功")
     server.wait_for_termination()
 
 
--- user_srv/settings/settings.py
+++ user_srv/settings/settings.py
@@ -15,4 +15,14 @@ MYSQL_PORT = 3306
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "123456"
 
+#consul的配置
+CONSUL_HOST = "172.20.0.204"
+CONSUL_PORT = 8500
+
+#服务相关的配置
+SERVICE_NAME = "user-srv"
+SERVICE_TAGS = ["imoc","mx"]
+
+
+
 DB = ReconnectMysqlDatabase(MYSQL_DB, host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, password=MYSQL_PASSWORD)
